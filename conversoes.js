fffb39bc139a854e6fbed9a10a002e44626d7bb5
MarceloPR <marceloprimi@gmail.com>
2023-12-05 10:36:22 -0300

    Aula de conversões

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado 
+// booleanos 
+
+// conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+
+console.log("A comparação dos numeros é " , numero === numeroString) // comparação com == ele compara o valor e com === ele compara o valor e o tipo, por exemplo em casos de comparação de string com numero
+
+
+//Number() Transformar um string num numero
+//String() Transformar um numero em string
+console.log("Somando as variaveis ", numero + Number(numeroString)) //Podemos adicionar esse number na hora de declaar o valor da variavel
+
+// conversão explícita
+
---
+++ truthy-falsy.js
@@ -0,0 +1,25 @@
+// boolean
+
+const usuarioLogado = true
+const contaPaga = false
+
+
+// truthy or false
+// 0 => false e 1 => true
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+// null e undefined
+// null => vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+
